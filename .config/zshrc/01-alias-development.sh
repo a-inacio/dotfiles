44fff217d586040a2e1460cb9836f465ec9be50c
António Inácio <ainacio@MacBook-Pro.lan>
2022-02-05 18:53:27 +0000

    chore: Update alias

--- .config/zshrc/01-alias-development.sh
+++ .config/zshrc/01-alias-development.sh
@@ -5,3 +5,44 @@ alias pl=pulumi
 alias plo="pulumi stack output -j | jq"
 
 alias dc="docker compose"
+
+# ------------------------------------------------------------------------------
+# Git
+# ------------------------------------------------------------------------------
+alias gs="git status"
+alias ga="git add . && git status"
+alias gp="git pull --rebase"
+alias gc="git commit"
+alias gP="git pull --rebase && git push"
+alias gd="git diff"
+alias gChor="git remote set-url origin"
+alias gAor="git remote add origin"
+alias gSu="git submodule update --remote --merge"
+alias gSi="git submodule update --init --recursive"
+
+# Undo add
+alias gUa="git restore --stage . && git status"
+
+# Undo commit, keep changes: https://stackoverflow.com/questions/15772134/can-i-delete-a-git-commit-but-keep-the-changes
+alias gUc="git reset HEAD^ && git status"
+
+# Push current branch to origin
+alias gPorr="git branch --show-current | xargs -I {} git -c 'push --set-upstream origin {}'"
+
+# ------------------------------------------------------------------------------
+# Yadm
+# ------------------------------------------------------------------------------
+alias ys="yadm status"
+alias ya="yadm add -u && yadm status"
+alias yp="yadm pull"
+alias yc="yadm commit"
+alias yP="yadm pull && yadm push"
+alias yd="yadm diff"
+alias ySu="yadm submodule update --remote --merge"
+alias ySi="yadm submodule update --init --recursive"
+
+# Undo add
+alias yUa="yadm restore --stage . && yadm status"
+
+# Undo commit
+alias yUc="yadm reset HEAD^ && yadm status"
