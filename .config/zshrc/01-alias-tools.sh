3121ad658eb00453effaf43731dccfe9c5f986a0
António Inácio <antonio.escobar.inacio@gmail.com>
2023-02-24 13:47:18 +0000

    feat: Yadm alias and improve brew fix detection

--- .config/zshrc/01-alias-tools.sh
+++ .config/zshrc/01-alias-tools.sh
@@ -5,4 +5,6 @@ alias gvim="Emacs"
 # alias brew86="arch -x86_64 /usr/local/homebrew/bin/brew"
 
 # Fixes issues with brew on M1
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if  [ -f "/opt/homebrew/bin/brew" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
